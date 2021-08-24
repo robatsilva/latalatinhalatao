8e5dcf0c79331c5a9aa01aa4ad314cb46388b07f
Robinson Silva <robinson.silva@linkconsulting.com.br>
2021-08-23 22:20:10 -0300

    aula10 - Adicionado botão de adicionar, por enquanto adiciona a garrafa 600 fixo

--- src/script.js
+++ src/script.js
@@ -26,26 +26,12 @@
       document.getElementById("lata473").style.display = 'flex';
 
     }
-    if (valorML == 600) {
-      document.getElementById("lata600").style.display = 'none'
-    } else {
-      document.getElementById("lata600").style.display = 'flex';
-
-    }
-    if (valorML == 1000) {
-      document.getElementById("lata1000").style.display = 'none'
-    } else {
-      document.getElementById("lata1000").style.display = 'flex';
-
-    }
   }
   function calcular() {
     let valorLata;
     let texto269;
     let texto350;
     let texto473;
-    let texto600;
-    let texto1000;
     let valorML;
     
     valorLata = document.getElementById('valorLata').value;
@@ -54,14 +40,10 @@
     texto269 = document.getElementById('texto269');
     texto350 = document.getElementById('texto350');
     texto473 = document.getElementById('texto473');
-    texto600 = document.getElementById('texto600');
-    texto1000 = document.getElementById('texto1000');
 
     texto350.innerText = regra3(valorML, 350, valorLata);
     texto473.innerText = regra3(valorML, 473, valorLata);
     texto269.innerText = regra3(valorML, 269, valorLata);
-    texto600.innerText = regra3(valorML, 600, valorLata);
-    texto1000.innerText = regra3(valorML, 1000, valorLata);
   }
 
   function regra3(v1G1, v2G1, v1G2) {
@@ -71,4 +53,18 @@
 
 
     return x.toFixed(2);
+  }
+
+  function adicionarLata(){
+    const lata = `
+                  <div class="lata" id='lata600'>
+                    <span>600 ml</span>
+                    <img src="../img/garrafa600.png">
+                    <p>R$ <span id="texto600">xx,xx</span></p>
+                  </div>
+                  `
+    const caixaLatas = document.getElementById('caixa-latas');
+    const textoExistente = caixaLatas.innerHTML;
+
+    caixaLatas.innerHTML = textoExistente + lata;
   }
